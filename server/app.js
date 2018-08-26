5cf9a64e504c51cdcc53316f348ba0c1d3ad89d6
e4rache <e4rache@protonmail.com>
2018-08-26 13:49:48 +0200

    .oOo.

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const schema = require('./schema/schema')
+const mongoose = require('mongoose')
+const config = require('./config.js')
+
+const app = express()
+
+mongoose.connect(config.MONGOOSE_CONNECT_URL, { useNewUrlParser: true })
+mongoose.connection.once('open', () => {
+    console.log('connected to database')
+})
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}))
+
+app.listen(4000, () => {
+    console.log('now listening for request on port 4000')
+})
+
